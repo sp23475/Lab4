8b72a70c2668b58e9746e77c32bff9068dac3623
sp23475 <sp23475@zut.edu.pl>
2020-06-29 23:17:14 +0200

    Lab5 - Usuwanie wpisów

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -51,6 +51,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView name = (TextView) view.findViewById(android.R.id.text1);
+                db.pobierz(Integer.parseInt(name.getText().toString()));
+                db.usun(name.getText().toString());
+                adapter.changeCursor(db.lista());
+                adapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+
+
+
     }
 
     @Override
